aa2f9f51471c32660ed27b7d6a4c58376dcf194e
伊東大樹 <itouhiroki@itoudaikinoMacBook-puro.local>
2018-11-27 15:17:51 +0900

    hello

--- FirstExperience/ViewController.swift
+++ FirstExperience/ViewController.swift
@@ -8,11 +8,26 @@
 
 import UIKit
 
+import MapKit
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //MapViewを生成し、表示する
+        let myMapView = MKMapView()
+        myMapView.frame = self.view.frame
+        self.view.addSubview(myMapView)
+        
+        //長押しを探知する機能を追加
+        //ジェスチャーの生成
+        let longPressGesture = UILongPressGestureRecognizer()
+        //ボタンを押したときの処理
+        longPressGesture.addTarget(self, action: "longPressed:")
+        myMapView.addGestureRecognizer(longPressGesture)
+
     }
 
     override func didReceiveMemoryWarning() {
